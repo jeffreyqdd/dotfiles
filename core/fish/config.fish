9b896b81722867ac313fb33b154c045014826641
Jeffrey Qian <jeffreyqdd@gmail.com>
2026-02-17 21:22:24 -0500

    feat: remove pyenv

--- core/fish/config.fish
+++ core/fish/config.fish
@@ -1,40 +1,5 @@
 # ========= Core Fish Configuration =========
 
-# ========= Aliases =========
-alias reattach="tmux attach-session"
-alias ll="ls -l"
-alias la="ls -a"
-alias lla="ls -la"
-
-# ========= Git Abbreviations =========
-abbr -a -- gs 'git status'
-abbr -a -- gc 'git commit'
-abbr -a -- gf 'git fetch'
-abbr -a -- gp 'git push'
-abbr -a -- gca 'git commit --amend --reset-author'
-abbr -a -- gco 'git checkout'
-abbr -a -- gcane 'git commit --amend --no-edit --reset-author'
-abbr -a -- gpf 'git push --force-with-lease'
-abbr -a -- gcanegpf 'git commit --amend --no-edit --reset-author && git push --force-with-lease'
-abbr -a -- gad 'git add .'
-
-# ========= Conditional Tool Init =========
-if command -q thefuck
-    thefuck --alias f | source
-end
-
-if command -q pyenv
-    pyenv init - fish | source
-end
-
-if test -f "$HOME/.cargo/env.fish"
-    source "$HOME/.cargo/env.fish"
-end
-
-if test -f "$HOME/.local/bin/env.fish"
-    source "$HOME/.local/bin/env.fish"
-end
-
 # ========= System-Specific Config =========
 set -g DOTFILES_DIR (realpath (status dirname)/../..)
 
@@ -47,6 +12,43 @@ switch (uname -s)
         set -g DOTFILES_SYSTEM unknown
 end
 
-if test -f "$DOTFILES_DIR/systems/$DOTFILES_SYSTEM/fish/system.fish"
-    source "$DOTFILES_DIR/systems/$DOTFILES_SYSTEM/fish/system.fish"
+if test -f "$DOTFILES_DIR/dotfiles/systems/$DOTFILES_SYSTEM/fish/system.fish"
+    source "$DOTFILES_DIR/dotfiles/systems/$DOTFILES_SYSTEM/fish/system.fish"
 end
+
+# ========= Aliases =========
+alias reattach="tmux attach-session"
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# ========= Git Abbreviations =========
+abbr -a -- gs 'git status'
+abbr -a -- gc 'git commit'
+abbr -a -- gf 'git fetch'
+abbr -a -- gp 'git push'
+abbr -a -- gca 'git commit --amend --reset-author'
+abbr -a -- gco 'git checkout'
+abbr -a -- gb 'git branch'
+abbr -a -- gcane 'git commit --amend --no-edit --reset-author'
+abbr -a -- gpf 'git push --force-with-lease'
+abbr -a -- gcanegpf 'git commit --amend --no-edit --reset-author && git push --force-with-lease'
+abbr -a -- gad 'git add .'
+
+# ========= Conditional Tool Init =========
+if command -q thefuck
+    thefuck --alias f | source
+end
+
+if test -f "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
+
+if test -f "$HOME/.local/bin/env.fish"
+    source "$HOME/.local/bin/env.fish"
+end
+
